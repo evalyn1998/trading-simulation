fd8c82a59849fe5aa2dabbfc2001783026692900
evalyn1998 <83055868+evalyn1998@users.noreply.github.com>
2025-10-18 10:46:15 +0800

    Inital commit

---
+++ src/main/java/org/example/tradingsimulation/TradingSimulationApplication.java
@@ -0,0 +1,13 @@
+package org.example.tradingsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingSimulationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tradingsimulation/TradingSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tradingsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
